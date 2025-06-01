990017d08cdff6fa475c1aeecda961c5b3c6fec0
lleebrooke <leebro@oregonstate.edu>
2025-06-01 11:38:56 -0700

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -12,6 +12,43 @@
 #include <cmath>
 
 double mortgage(){
+int mainPrice;
+int mainDown;
+int mainLoan;
+double mainInterest;
+double mainTax;
+  
+        std::cout << "Enter the home price, in dollars: ";
+        std::cin >> mainPrice;
+      
+        std::cout << "Enter the down payment, in dollars: ";
+        std::cin >> mainDown;
+  
+        std::cout << "Enter the length of loan, in months: ";
+        std::cin >> mainLoan;
+  
+        std::cout << "Enter the Monthly Interest Rate, in percentage: ";
+        std::cin >> mainInterest;
+
+        std::cout << "Enter the property tax rate, in percentage: ";
+        std::cin >> mainTax;
+
+       double mortgagePrinciple = mainPrice-mainDown;
+       double monthlyTax = ((mainTax * mainPrice) / 12)/100;
+       double monthlyInsurance = ((mainInsurance*mainPrice) / 12)/100;
+       double num = pow(1+(mainInterest/100),mainLoan);
+      double monthlyMortgage = ((mortgagePrinciple*((mainInterest/100)*num)) / (num-1)) +(monthlyTax + monthlyInsurance);
+	       
+	std::cout << "Your estimated monthly payment is: $" << monthlyMortgage << "\n";
+
+	double totalPaid = (monthlyMortgage*mainLoan);
+	std::cout << "Total paid: $" << totalPaid << "\n" ;
+	std::cout << "Principal paid: $" << mortgagePrinciple << "\n" ;
+	
+	double additionalPaid = (totalPaid-mortgagePrinciple); 
+	std::cout << "Additional paid: $" << additionalPaid << "\n";
+	std::cout << "Thank you for using the mortgage calculator." << "\n";
+
 for (int year = 1; year <= 30; ++year) {
   // perform yearly updates
 for (int month = 1; month <= 12; ++month) {
